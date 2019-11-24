7b46e66dda7948a4a7f37b89cab4152b7ad997e9
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-24 18:49:26 +0100

    Update Client.js

--- src/client/Client.js
+++ src/client/Client.js
@@ -1,12 +1,40 @@
+const Player = require("../structures/Player.js");
+const KrunkerAPIError = require("../errors/KrunkerAPIError.js");
 const ws = require("ws");
+const {encode, decode} = require("msgpack-lite");
 
 module.exports = class {
     constructor () {
+    }
+    connectToSocket () {
         this.ws = new ws("wss://krunker_social.krunker.io/ws", {
             handshakeTimeout: 10000
         });
     }
-    disconnect () {
-        if (this.ws.readyState === 1) this.ws.close();
+    disconnectFromSocket () {
+        if (this.ws && this.ws.readyState === 1) this.ws.close();
+    }
+    
+    fetchProfile (username) {
+        if (!username) throw new Error("No username given.");
+        
+        this.connectToSocket();
+        
+        return new Promise((res, rej) => {
+            this.ws.onopen = () => this.ws.send(encode(["r", ["profile", username, "000000", null]]).buffer);
+            this.ws.onerror = (err) => {
+                this.ws.terminate();
+                rej(err);
+            };
+            
+            this.socket.onmessage = buffer => {
+                const userData = decode(new Uint8Array(buff.data))[1][2];
+                this.disconnectFromSocket();
+                
+                if (!userData || !userData.player_stats) return rej(new KrunkerAPIError("Player not found"));
+                
+                resolve(new Player(userData));
+            };
+        });
     }
 }
