b438c934be699c7f86d30253ab3f5eceefdb3493
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-24 18:53:45 +0100

    Update Client.js

--- src/client/Client.js
+++ src/client/Client.js
@@ -33,7 +33,7 @@ module.exports = class {
                 
                 if (!userData || !userData.player_stats) return rej(new KrunkerAPIError("Player not found"));
                 
-                resolve(new Player(userData));
+                resolve(new Player(JSON.parse(userData)));
             };
         });
     }
