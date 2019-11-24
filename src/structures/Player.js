b8b7fcb8c9659dc4b0496d711a72d3304cd0cebc
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-24 19:25:48 +0100

    Update Player.js

--- src/structures/Player.js
+++ src/structures/Player.js
@@ -6,9 +6,23 @@ module.exports = class {
         
         return {
             username: data.player_name,
+            level: Math.max(1, Math.floor(0.03 * Math.sqrt(data.player_score))),
+            levelProgress: Math.round(100 * ((0.03 * Math.sqrt(data.player_score)) - Math.floor(0.03 * Math.sqrt(data.player_score)))),
+            score: data.player_score,
             displayName: (data.player_clan ? data.player_name + " [" + data.player_clan + "]" : data.player_name)
             id: data.player_id,
+            lastPlayedClass: classes[stats.c],
             stats: {
+                timePlayed: {
+                    mins: Math.floor(Math.floor(data.player_timeplayed / 1000) / 60) % 60,
+                    hours: Math.floor(Math.floor(Math.floor(data.player_timeplayed / 1000) / 60) / 60) % 24,
+                    days: Math.floor(Math.floor(Math.floor(Math.floor(data.player_timeplayed / 1000) / 60) / 60) / 24),
+            
+                    toString(){
+                        return Math.floor(Math.floor(Math.floor(data.player_timeplayed / 1000) / 60) / 60) % 24 + "h " + Math.floor(Math.floor(data.player_timeplayed / 1000) / 60) % 60 + "m"
+                    }
+                },
+            
                 shots: stats.s,
                 hits: stats.h,
                 accuracy: stats.s / stats.h,
@@ -22,14 +36,15 @@ module.exports = class {
                 gamesPlayed: data.player_games_played,
                 wins: data.player_wins,
                 losses: data.player_games_played - data.player_wins,
-                wlr: (data.player_wins / (data.player_games_played - data.player_wins)).toFixed(2)
+                wlr: (data.player_wins / (data.player_games_played - data.player_wins)).toFixed(2),
+            
+                kpg: (data.player_games_played / data.player_kills).toFixed(2)
             },
             social: {
                 clan: data.player_clan ? data.player_clan : null,
                 following: data.player_following || 0,
                 followers: data.player_followed || 0,
-            },
-            
+            }
         };
     }
 }
