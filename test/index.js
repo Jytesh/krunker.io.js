8a53e52a6c55ed6f71f3ce6d3ec0db515a0cc04e
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-25 20:09:09 +0100

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,8 +1,33 @@
 const Krunker = require("krunker.js");
 const client = new Krunker.Client();
 
-(async () => console.log(await client.fetchPlayer("1s3k3b"))); // fetch methods return a Promise as it takes time for the socket to load data, you need to wait for it to resolve in order to work
+(async () => console.log(await client.fetchPlayer("1s3k3b")))(); // fetch methods return a Promise as it takes time for the socket to load data, you need to wait for it to resolve in order to work
 
 /*
-    
+Player {
+    username: "1s3k3b",
+    level: 20,
+    levelProgress: 44060,
+    score: 488505,
+    displayName: "1s3k3b [Vixe]",
+    id: "no",
+    lastPlayedClass: Class {
+        name: "Hunter",
+        health: 60,
+        mainWpn: Weapon {
+            name: "Sniper",
+            damage: "no idea",
+            ammo: 3,
+            reloadTime: "no idea"
+        }   
+    },
+    stats: {
+        timePlayed: {
+            mins: 69,
+            hours: 69,
+            days: 69,
+            toString: fn => "69h 69m"
+        }
+    }
+}
 */
