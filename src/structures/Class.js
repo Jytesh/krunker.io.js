b7a875b0d654d520427312233bcbd2d0c58628f8
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-26 20:39:53 +0100

    Update Class.js

--- src/structures/Class.js
+++ src/structures/Class.js
@@ -6,28 +6,7 @@ module.exports = class {
             health: 100,
             name: "Triggerman",
             secondary: true,
-            weapon: {
-                swapTime: 300,
-                aimSpeed: 130,
-                speedMultiplier: 0.95,
-                ammo: 30,
-                reloadTime: 1200,
-                damage: {
-                    dropoff: 5,
-                    damage: 23,
-                    toNumber(){
-                        return 23;
-                    }
-                    toString(){
-                        return "23";
-                    }
-                },
-                range: 700,
-                rateOfFire: 110,
-                spread: 100,
-                zoom: 1.6,
-                recoil: 0.003
-            }
+            weapon: new Weapon("Assault Rifle")
         };
         
         const hunter = {
