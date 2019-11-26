8bad4299b700fbdc9c2bef1c3e66746a079f45af
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-26 19:28:33 +0100

    Update Class.js

--- src/structures/Class.js
+++ src/structures/Class.js
@@ -2,31 +2,58 @@ const Weapon = require("./Weapon.js");
 
 module.exports = class {
     constructor (name = "Triggerman") {
-        return {
-            "Triggerman": {
-                health: 100,
-                name: "Triggerman",
-                secondary: true,
-                weapon: {
-                    swapTime: 300,
-                    aimSpeed: 130,
-                    speedMultiplier: 0.95,
-                    ammo: 30,
-                    reloadTime: 1200,
-                    damage: {
-                        dropoff: 5,
-                        damage: 23,
-                        toNumber(){
-                            return 23;
-                        }
-                    },
-                    range: 700,
-                    rateOfFire: 110,
-                    spread: 100,
-                    zoom: 1.6,
-                    recoil: 0.003
-                }
+        const triggerman = {
+            health: 100,
+            name: "Triggerman",
+            secondary: true,
+            weapon: {
+                swapTime: 300,
+                aimSpeed: 130,
+                speedMultiplier: 0.95,
+                ammo: 30,
+                reloadTime: 1200,
+                damage: {
+                    dropoff: 5,
+                    damage: 23,
+                    toNumber(){
+                        return 23;
+                    }
+                    toString(){
+                        return "23";
+                    }
+                },
+                range: 700,
+                rateOfFire: 110,
+                spread: 100,
+                zoom: 1.6,
+                recoil: 0.003
             }
-        }[name];        
+        };
+        
+        const hunter = {
+            health: 60,
+            name: "Hunter",
+            secondary: true,
+            weapon: {
+                swapTime: 300,
+                aimSpeed: 120,
+                speedMultiplier: 0.95,
+                ammo: 3,
+                reloadTime: 1500,
+                damage: {
+                    dropoff: 30,
+                    damage: 100
+                },
+                range: 1000
+                rateOfFire: 900,
+                spread: 260,
+                zoom: 2.7,
+                recoil: 0.009
+            }
+        };
+        
+        return {
+            triggerman, hunter, runNgun
+        }[name.toLowerCase()];        
     }
 }
