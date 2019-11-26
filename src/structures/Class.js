d843cc33beaa8a0a7e7e22138e58230acfdd732a
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-26 20:44:14 +0100

    Update Class.js

--- src/structures/Class.js
+++ src/structures/Class.js
@@ -13,22 +13,14 @@ module.exports = class {
             health: 60,
             name: "Hunter",
             secondary: true,
-            weapon: {
-                swapTime: 300,
-                aimSpeed: 120,
-                speedMultiplier: 0.95,
-                ammo: 3,
-                reloadTime: 1500,
-                damage: {
-                    dropoff: 30,
-                    damage: 100
-                },
-                range: 1000
-                rateOfFire: 900,
-                spread: 260,
-                zoom: 2.7,
-                recoil: 0.009
-            }
+            weapon: new Weapon("Sniper Rifle")
+        };
+        
+        const runNgun = {
+            health: 100,
+            name: "Run N Gun",
+            secondary: false,
+            weapon: new Weapon("Submachine Gun")
         };
         
         return {
