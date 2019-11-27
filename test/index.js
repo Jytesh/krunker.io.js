2abdff24895fcd35c60951e678356c079b67badf
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-27 18:10:22 +0100

    Update index.js

--- test/index.js
+++ test/index.js
@@ -12,21 +12,43 @@ Player {
     displayName: "1s3k3b [Vixe]",
     id: "no",
     lastPlayedClass: Class {
-        name: "Hunter",
-        health: 60,
-        mainWpn: Weapon {
-            name: "Sniper",
-            damage: "no idea",
-            ammo: 3,
-            reloadTime: "no idea"
-        }   
+        name: "Run N Gun",
+        health: 100,
+        secondary: false,
+        weapon: Weapon {
+            name: "Submachine Gun",
+            class: Class {
+                ...Run N Gun
+            },
+            swapTime: 300,
+            aimSpeed: 120,
+            speedMultiplier: 1.04,
+            ammo: 24,
+            reloadTime: 1000,
+            damage: {
+                damage: 18,
+                dropoff: 12,
+                toNumber: fn 18
+                toString: fn "18"
+            },
+            range: 700,
+            rateOfFire: 70,
+            spread: 70,
+            zoom: 1.65,
+            recoil: 0.0034,
+            automatic: true,
+            baseScore: 50,
+            sight: "Red Dot",
+            toString: fn "Submachine Gun"
+        },
+        toString: fn "Run N Gun"
     },
     stats: {
         timePlayed: {
             mins: 69,
             hours: 69,
             days: 69,
-            toString: fn => "69h 69m"
+            toString: fn "69h 69m"
         }
     }
 }
