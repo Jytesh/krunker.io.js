091b7a153192b9a5309612d011e970996a74a234
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-28 18:13:04 +0100

    Create Game.js

---
+++ src/structures/Game.js
@@ -0,0 +1,33 @@
+// from my BetterJS
+Object.prototype.forEach = function (callback) {
+    Object.keys(this).forEach((key, index) => {
+        callback(key, this[key], index, this);
+    });
+}
+
+
+module.exports = class Game {
+    constructor (data) {
+        const obj = {
+            players: {
+                players: data.clientCount,
+                max: data.maxClients,
+                toString () {
+                    return this.players + "/" + this.max;
+                }
+                toNumber () {
+                    return this.players;
+                }
+            },
+            gameMode: data.i.split("_")[0],
+            map: data.i.split("_")[1],
+            custom: data.cs
+        };
+        
+        obj.forEach((k, v) => {
+            this[k] = v;
+        });
+        
+        return obj;
+    }
+}
