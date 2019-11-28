d09ca5cb71d3f97115e7cc4217c9124bebb97f97
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-28 17:11:04 +0100

    Update PlayerClient.js

--- src/client/PlayerClient.js
+++ src/client/PlayerClient.js
@@ -1,9 +1,61 @@
 const EventEmitter = require("events");
 
+const Player = require("../structures/Player.js");
+const Client = require("./Client.js");
+
+// from my BetterJS
+Object.prototype.forEach = function (callback) {
+    Object.keys(this).forEach((key, index) => {
+        callback(key, this[key], index, this);
+    });
+}
+
+const getDifference = (obj1, obj2) => {
+    const differences = {};
+    
+    obj1.forEach((k, v) => {
+        if (obj1[k] !== obj2[k]) differences[k] = {
+            old: obj1[k],
+            new: obj2[k]
+        };
+    });
+    
+    return differences;
+};
+
 /*
  * A Client for a single player, including a lot of events.
  * @extends {EventEmitter}
  */
-module.exports = class PlayerClient {
-
+module.exports = class PlayerClient extends EventEmitter {
+    /*
+     * @param {string|Player} usernameOrPlayer the desired player's username or Player object
+     */
+    async constructor (usernameOrPlayer = "1s3k3b") {
+        super();
+        /*
+         * The client's player.
+         * @private
+         * @type {Player}
+         */
+        this._player = await new Client().fetchPlayer(usernameOrPlayer.username);
+        
+        if (!this._player) return;
+        
+        setInterval(async () => {
+            const oldPlayer = this._player;
+            
+            this._player = await new Client().fetchPlayer(usernameOrPlayer.username);
+            
+            if (oldPlayer === this._player) return;
+            
+            const diff = getDifference(oldPlayer, this._player);
+            
+            if (diff.level) this.emit("levelUp", diff.level.new);
+            if (diff.lastPlayedClass) this.emit("classSwitch", diff.lastPlayedClass.old, diff.lastPlayedClass.new);
+            if (diff.stats) {
+            
+            }
+        }, 1000);
+    }
 }
