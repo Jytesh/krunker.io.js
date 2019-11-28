96241d6d347ef209ffa8e7c47241f165bfde498b
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-28 16:42:22 +0100

    Create PlayerClient.js

---
+++ src/client/PlayerClient.js
@@ -0,0 +1,9 @@
+const EventEmitter = require("events");
+
+/*
+ * A Client for a single player, including a lot of events.
+ * @extends {EventEmitter}
+ */
+module.exports = class PlayerClient {
+
+}
