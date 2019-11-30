fbd20db6d7a5ed0f5eb66c2a228e9449cc247588
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-30 20:15:06 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     Client: require("./client/Client.js"),
+    PlayerClient: require("./client/PlayerClient.js"),
     Class: require("./structures/Class.js"),
     Weapon: require("./structures/Weapon.js"),
     regExp: {
