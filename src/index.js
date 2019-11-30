9c60bc82341ff34ddbfa019d690641cc375b4d62
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-30 19:29:39 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 module.exports = {
     Client: require("./client/Client.js"),
     Class: require("./structures/Class.js"),
-    Weapon: require("./structures/Weapon.js")
+    Weapon: require("./structures/Weapon.js"),
+    regExp: {
+        gameID: /[A-Z]{2,}:[abcdefghijklmnopqrstuvwxyz0123456789]+/g
+    }
 };
