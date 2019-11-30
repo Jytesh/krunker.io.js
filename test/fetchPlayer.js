91b870eb6c7feb034ee59e203cb244e4cd09d1eb
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-11-30 19:49:38 +0100

    Update fetchPlayer.js

--- test/fetchPlayer.js
+++ test/fetchPlayer.js
@@ -1,4 +1,4 @@
-const Krunker = require("../src/client/Client.js");
+const Krunker = require("../src/index.js");
 const client = new Krunker.Client();
 
 (async () => console.log(await client.fetchPlayer("1s3k3b")))(); // fetch methods return a Promise as it takes time for the socket to load data, you need to wait for it to resolve in order to work
