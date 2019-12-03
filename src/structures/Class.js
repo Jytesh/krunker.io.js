f16894685ef73c0ab9ed964eb0072546f8bac7ca
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-03 18:38:17 +0100

    Update Class.js

--- src/structures/Class.js
+++ src/structures/Class.js
@@ -49,8 +49,75 @@ module.exports = class Class {
             }
         };
         
+        const vince = {
+            health: 100,
+            name: "Vince",
+            secondary: true,
+            weapon: new Weapon("Shotgun"),
+            toString(){
+                return "Vince";
+            }
+        };
+        
+        const detective = {
+            health: 100,
+            name: "Detective",
+            secondary: false,
+            weapon: new Weapon("Revolver"),
+            toString(){
+                return "Detective";
+            }
+        };
+        
+        const marksman = {
+            health: 90,
+            name: "Marksman",
+            secondary: true,
+            weapon: new Weapon("Semi Auto"),
+            toString(){
+                return "Marksman";
+            }
+        };
+        
+        const rocketeer = {
+            health: 130,
+            name: "Rocketeer",
+            secondary: true,
+            weapon: new Weapon("Rocket Launcher"),
+            toString(){
+                return "Rocketeer";
+            }
+        };
+    
+        const agent = {
+            health: 100,
+            name: "Agent",
+            secondary: false,
+            weapon: new Weapon("Akimbo Uzi"),
+            toString(){
+                return "Agent";
+            }
+        };
+        
+        const runner = {
+            health: 100,
+            name: "Runner",
+            secondary: false,
+            weapon: new Weapon("Combat Knife"),
+            toString(){
+                return "Runner";
+            }
+        };
+        
+        const bowman = {
+            health: 100,
+            name: "Bowman",
+            secondary: true,
+            weapon: new Weapon("Crossbow")
+        };
+        
         const obj = {
-            triggerman, hunter, runngun, spraynpray
+            triggerman, hunter, runngun, spraynpray, vince, detective, marksman, rocketeer, agent, runner, bowman, commando
         }[name.split(" ").join("").toLowerCase()];
         
         obj.forEach((k, v) => {
