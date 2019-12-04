2f3a507b56c192e9999f1b30a7267641c562c62b
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-04 16:13:25 +0100

    Update resolver.js

--- src/client/resolver.js
+++ src/client/resolver.js
@@ -9,8 +9,16 @@ module.exports = {
         arr = Array.from(new Set(arr));
         if (arr.some(v => Boolean(new Class(v)))) return arr.filter(v => Boolean(new Class(v)));
         if (arr.some(v => v instanceof Class && new Class(v.name))) return arr.filter(v => v instanceof Class && new Class(v.name)).map(v => v.name);
-        if (arr.some(v => Boolean(new Weapon(v)))) return arr.filter(v => Boolean(new Class(v))).map(w => w.name);
+        if (arr.some(v => Boolean(new Weapon(v)))) return arr.filter(v => Boolean(new Class(v))).map(w => w.class);
         if (arr.some(v => v instanceof Weapon && new Weapon(v.name))) return arr.filter(v => v instanceof Weapon && new Weapon(v.name)).map(w => w.class);
+        return classArr;
+    },
+    weaponNameArray(arr) {
+        arr = Array.from(new Set(arr));
+        if (arr.some(v => Boolean(new Weapon(v)))) return arr.filter(v => Boolean(new Weapon(v)));
+        if (arr.some(v => v instanceof Weapon && new Weapon(v.name))) return arr.filter(v => v instanceof Weapon && new Weapon(v.name)).map(v => v.name);
+        if (arr.some(v => Boolean(new Class(v)))) return arr.filter(v => Boolean(new Class(v))).map(c => c.weapon.name);
+        if (arr.some(v => v instanceof Class && new Class(v.weapon.name))) return arr.filter(v => v instanceof Class && new Class(v.weapon.name)).map(w => w.weapon.name);
         return classArr;
     }
 };
