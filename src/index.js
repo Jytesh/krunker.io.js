a601fc5c4c85aa2ed0cb842ee6697f7b2b2cbe56
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-04 15:08:10 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,7 @@
-const Class = require("./structures/Class.js");
-const Weapon = require("./structures/Weapon.js");
-
-const avg = (...nums) => nums.reduce((acc, val) => acc + val) / nums.length;
-
-const classes = ["Triggerman", "Hunter", "Run N Gun", "Spray N Pray", "Vince", "Detective", "Marksman", "Rocketeer", "Agent", "Runner", "Bowman", "Commando"];
-
 module.exports = {
     Client: require("./client/Client.js"),
-    Class,
-    Weapon,
-    util: {
-        classes,
-        gameIDregExp: /[A-Z]{2,}:[a-z0-9]]+/g,
-        averageStat(structure, stat) {
-            if (!structure) return;
-            structure = structure.trim().toLowerCase();
-            switch (structure) {
-                case "class":
-                    const exampleStat = new Class("Triggerman")[stat];
-                    if (!exampleStat) break;
-                    if (typeof exampleStat === "number") return avg(...classes.map(n => new Class(n)[stat]));
-            }
-        }
-    }
+    Class: require("./structures/Class.js"),
+    Weapon: require("./structures/Weapon.js"),
+    Collection: require("./util/Collection.js"),
+    util: require("./util/index.js"),
 };
