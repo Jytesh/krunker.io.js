f440e082a9f6bc8ce7e7b8653b500730ff9c1a4e
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-04 16:08:01 +0100

    Update resolver.js

--- src/client/resolver.js
+++ src/client/resolver.js
@@ -7,5 +7,6 @@ module.exports = {
         if (arr.some(v => Boolean(new Class(v)))) return arr.filter(v => Boolean(new Class(v)));
         if (arr.some(v => v instanceof Class && new Class(v.name))) return arr.filter(v => v instanceof Class && new Class(v.name)).map(v => v.name);
         if (arr.some(v => Boolean(new Weapon(v)))) return arr.filter(v => Boolean(new Class(v))).map(w => w.name);
+        if (arr.some(v => v instanceof Weapon && new Weapon(v.name))) return arr.filter(v => v instanceof Weapon && new Weapon(v.name)).map(w => w.class);
     }
 };
