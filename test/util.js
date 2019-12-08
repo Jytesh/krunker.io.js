97b23191765c4ec8798c3f269dfa5c9006f1c069
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-08 12:48:53 +0100

    Update util.js

--- test/util.js
+++ test/util.js
@@ -3,3 +3,6 @@ const { util } = require("../src/index.js");
 console.log("Get the average damage of all the weapons");
 console.log(util.averageStat("weapon", "damage"));
 // 44.58
+
+console.log("Get our chances for getting a contraband from heroic spins with 20k KR");
+console.log(util.spinChance("heroic", "contraband", 20000));
