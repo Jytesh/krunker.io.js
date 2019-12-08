a0c9d061aa1e827960235b1f6bd103f21e906713
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-08 12:52:20 +0100

    Update util.js

--- test/util.js
+++ test/util.js
@@ -1,8 +1,15 @@
 const { util } = require("../src/index.js");
 
-console.log("Get the average damage of all the weapons");
-console.log(util.averageStat("weapon", "damage"));
+console.log("The average damage of all weapons in the game:");
+console.log(util.averageStat("weapon", "damage"), "\n");
 // 44.58
 
-console.log("Get our chances for getting a contraband from heroic spins with 20k KR");
-console.log(util.spinChance("heroic", "contraband", 20000));
+console.log("Our chances for getting a contraband from heroic spins with 20k KR:");
+console.log(util.spinChance("heroic", "contraband", 20000), "\n");
+// 20.00
+
+console.log("An array of all classes' names in the game:");
+console.log(util.classes, "\n");
+
+console.log("An array of all weapons' names in the game:");
+console.log(util.weapons, "\n");
