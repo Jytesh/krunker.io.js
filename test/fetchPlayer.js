87a7bf6a149a0b0439840fd74dc7a40163ab76d4
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-08 12:46:16 +0100

    Update fetchPlayer.js

--- test/fetchPlayer.js
+++ test/fetchPlayer.js
@@ -4,8 +4,6 @@ const client = new Krunker.Client();
 (async () => console.log(await client.fetchPlayer("1s3k3b")))(); // fetch methods return a Promise as it takes time for the socket to load data, you need to wait for it to resolve in order to work
 
 /*
-output in console:
-
 {
   username: '1s3k3b',
   level: 21,
