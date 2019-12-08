9d449d3e912c9c1b8b35d8f756d46311a92231e5
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-08 12:41:30 +0100

    Update index.js

--- src/util/index.js
+++ src/util/index.js
@@ -101,7 +101,8 @@ module.exports = {
                 var exampleStat = new Weapon("Assault Rifle")[stat];
                 if (!exampleStat) break;
                 arr = arr ? resolver.weaponNameArray(arr) : weapons;
-                if (typeof Number(exampleStat) === "number") return avg(...arr.map(n => Number(new Weapon(n)[stat])));
+                if (exampleStat.toNumber && typeof exampleStat.toNumber() === "number") return avg(...arr.map(n => new Weapon(n)[stat].toNumber()));
+                if (typeof exampleStat === "number") return avg(...arr.map(n => new Weapon(n)[stat]));
                 return mostOccurs(arr.map(n => new Class(n)[stat])).element;
             default: return void console.error("Invalid structure " + structure);
         }
