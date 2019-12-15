ab04c2bf5fbb5d196b5f898f56959fe5c114a2e1
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-15 11:44:19 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ module.exports = {
     Client: require("./client/Client.js"),
     Class: require("./structures/Class.js"),
     Weapon: require("./structures/Weapon.js"),
-    Collection: require("./util/Collection.js"),
     util: require("./util/index.js")
 };
