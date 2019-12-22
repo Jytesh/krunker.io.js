4bed67b2c5c0915d5a3dd42ebad508fd4365810b
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-22 21:19:13 +0100

    Update typings.d.ts

--- typings/typings.d.ts
+++ typings/typings.d.ts
@@ -1,5 +1,12 @@
 declare module "krunker.io.js" {
-    export interface Client {
+    declare interface Class {
+        health: number;
+        name: string;
+        secondary: boolean;
+        weapon: Weapon;
+        toString(): string;
+    }
+    declare interface Client {
         private _connectToSocket(): void;
         private _disconnectFromSocket(): void;
         public fetchPlayer(username: string): Promise<Player>;
@@ -7,4 +14,6 @@ declare module "krunker.io.js" {
         public fetchChangelog(): Promise<Changelog>;
         public getPlayer(nameOrID: string): Player|Promise<Player>;
     }
+
+    export default Client;
 }
