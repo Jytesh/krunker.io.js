47cbb1171b7308c89e85e8923655f457fd460ad2
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-22 20:51:48 +0100

    Update Changelog.js

--- src/structures/Changelog.js
+++ src/structures/Changelog.js
@@ -3,6 +3,6 @@ const Version = require("./Version.js");
 module.exports = class Changelog {
     constructor(text) {
         this.versions = text.split("\n\n").map(v => new Version(v));
-        this.latestVersion = versions[0].version;
+        this.latestVersion = this.versions[0].version;
     }
 }
