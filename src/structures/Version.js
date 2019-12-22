c0099e5c0fefd2c1f664925103cc8b18509a850e
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-22 20:23:00 +0100

    Update Version.js

--- src/structures/Version.js
+++ src/structures/Version.js
@@ -1,3 +1,6 @@
-module.exports = {
-    
+module.exports = class Version {
+    constructor(text) {
+        this.version = text.split("\n")[0].replace(/[^0-9.]/g, "");
+        this.changes = text.split("\n").slice(1).map(x => x.substring(2));
+    }
 };
