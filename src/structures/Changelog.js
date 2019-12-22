787530b1f03122808f5a8352d756bb789bb8c194
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-22 20:33:17 +0100

    Update Changelog.js

--- src/structures/Changelog.js
+++ src/structures/Changelog.js
@@ -2,6 +2,7 @@ const Version = require("./Version.js");
 
 module.exports = class Changelog {
     constructor(text) {
+        console.log(text);
         text = text.match(/==(.*)==\n([\s\S])+/g)[0];
         this.versions = text.split("\n\n").map(v => new Version(v));
         this.latestVersion = versions[0].version;
