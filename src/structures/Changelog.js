4c88a54094e327de29b5ed0a9f1046618a9f59ca
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-23 15:23:00 +0100

    Update Changelog.js

--- src/structures/Changelog.js
+++ src/structures/Changelog.js
@@ -1,9 +1,18 @@
-const Version = require("./Version.js");
-
 module.exports = class Changelog {
     constructor(text) {
-        this.versions = text.split("\n\n").filter(x => x.split("\n")[0].split("").some(y => !isNaN(Number(y)))).map(v => new Version(v));
-        this.latestUpdate = this.versions[0];
-        this.currentVersion = this.versions[0].version;
+        this.raw = text;
+        
+        let arr = [];
+        text.split("\n").filter(x => x).forEach(x => {
+            if (x.startsWith("=")) return arr.push({
+                version: x.match(/([0-9].?){3}/g) ? x.match(/([0-9].?){3}/g)[0] : "",
+                changes: [] 
+            });
+            arr[arr.length - 1] && arr[arr.length - 1].changes.push(x);
+        });
+        arr = arr.filter(x => x.version);
+        
+        this.versions = arr;
+        this.latestVersion = arr[0];
     }
 }
