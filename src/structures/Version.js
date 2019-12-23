33310dd90dd8f3c3caf5cfb920e3446d1bbd9367
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-23 15:23:22 +0100

    Delete Version.js

--- src/structures/Version.js
+++
@@ -1,6 +0,0 @@
-module.exports = class Version {
-    constructor(text) {
-        this.version = text.split("\n")[0].replace(/[^0-9.]/g, "");
-        this.changes = text.split("\n").slice(1).map(x => x.substring(2));
-    }
-};
