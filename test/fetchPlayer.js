b804dc4b5a84d47609f53c570b409574d0929e1c
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2019-12-26 16:54:22 +0100

    Update fetchPlayer.js

--- test/fetchPlayer.js
+++ test/fetchPlayer.js
@@ -1,7 +1,7 @@
 const Krunker = require("../src/index.js");
 const client = new Krunker.Client();
 
-(async () => console.log(await client.fetchPlayer("1s3k3b")))(); // fetch methods return a Promise as it takes time for the socket to load data, you need to wait for it to resolve in order to work
+client.fetchPlayer("1s3k3b").then(console.log); // fetch methods return a Promise as it takes time for the socket to load data, you need to wait for it to resolve in order to work
 
 /*
 {
