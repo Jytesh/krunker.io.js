8f98a2b99b9b6c2703a5c93dfd2b3a773c36ec42
1s3k3b <56838314+1s3k3b@users.noreply.github.com>
2020-01-03 09:49:04 +0100

    Update Changelog.js

--- src/structures/Changelog.js
+++ src/structures/Changelog.js
@@ -5,7 +5,7 @@ module.exports = class Changelog {
         let arr = [];
         text.split("\n").filter(x => x).forEach(x => {
             if (x.startsWith(" == ")) return arr.push({
-                version: x.match(/([0-9].?){3}/) ? x.match(/([0-9].?){3}/)[0] : "",
+                version: x.match(/(\d{1,3}.?){3}/) ? x.match(/(\d{1,3}.?){3}/)[0] : "",
                 changes: [] 
             });
             arr[arr.length - 1] && arr[arr.length - 1].changes.push(x);
