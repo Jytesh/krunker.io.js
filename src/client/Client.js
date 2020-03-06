0a115c2f6a95bd90bb73c0764973da41978ca120
Bence <56838314+1s3k3b@users.noreply.github.com>
2020-03-06 20:17:45 +0100

    Update Client.js

--- src/client/Client.js
+++ src/client/Client.js
@@ -41,7 +41,7 @@ module.exports = class Client {
         return new Promise(async (res, rej) => {
             if (!name) return rej(new ArgumentError("No clan name given"));
             const r = await fetch("https://krunker.social/api?clan=" + name);
-            if (!r.ok) return rej(new KrunkerAPIError("Clan not found"));
+            if (!r.ok || (await r.json()).error) return rej(new KrunkerAPIError("Clan not found"));
             const c = new Clan(await r.json());
             if (cache) this.clans.set(c.name + "_" + c.id);
             res(raw ? await r.json() : c);
