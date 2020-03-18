d098cc1676fef3479253083965c53b4a71a44820
Bence <56838314+1s3k3b@users.noreply.github.com>
2020-03-18 13:42:58 +0100

    add Mod structure

---
+++ src/structures/Mod.js
@@ -0,0 +1,23 @@
+const Client = require("../client/Client.js");
+
+module.exports = class Mod {
+    constructor(data) {
+        this.name = data.mod_name;
+        this.authorUsername = data.creatorname;
+        this.rank = data.mod_rank;
+        this.id = data.mod_id;
+        this.url = data.mod_url;
+        this.votes = data.mod_votes;
+        this.createdAt = new Date(data.mod_date);
+        Object.defineProperty(this, "image", {
+            value: data.mod_image,
+            writable: true,
+            configurable: true
+        });
+    }
+    async fetchAuthor(client) {
+        if (client instanceof Client === false) client = new Client();
+        this.author = await client.fetchPlayer(this.authorUsername);
+        return this.author;
+    }
+}
