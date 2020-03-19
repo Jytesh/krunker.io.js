baa5311976d864d511c54a29ed22dd08595bbb66
Bence <56838314+1s3k3b@users.noreply.github.com>
2020-03-19 17:18:06 +0100

    Create leaderboard.js

---
+++ test/leaderboard.js
@@ -0,0 +1,7 @@
+const { Client } = require("../src/index.js");
+const client = new Client();
+
+client.fetchLeaderboard().then(c => {
+    console.log(c);
+    console.log(client.getLeaderboard());
+});
